c59e9ddf5a1c5cc410db712146da4b4c6111a69d
beztebya666 <nikivinn@gmail.com>
2026-03-07 16:31:37 +0300

    upd dockerfile

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/mongostudio/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'pages' ? '/mongostudio/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -22,4 +22,4 @@ export default defineConfig({
     sourcemap: false,
     minify: 'esbuild',
   },
-});
+}));
